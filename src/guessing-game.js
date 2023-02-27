1e683d0fd3727badc54836287c6e0753917f79d1
zeddlex1 <a.zeddlex@gmail.com>
2023-02-27 16:23:52 +0300

    fead: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,17 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() { }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return Math.ceil((this.max + this.min)/2);
     }
-
     lower() {
-
+        return this.max = Math.ceil((this.max + this.min)/2);
     }
-
     greater() {
-
+        return this.min = Math.ceil((this.max + this.min)/2);
     }
 }
-
 module.exports = GuessingGame;
